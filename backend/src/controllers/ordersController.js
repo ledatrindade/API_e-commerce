4fbf191b8e806f0f8be4d1ed14e63be8b116801f
Antonio.lopes <antonyejoao@gmail.com>
2024-10-23 10:33:13 -0300

    iniciando estrutura

--- backend/src/controllers/ordersController.js
+++ backend/src/controllers/ordersController.js
@@ -1,6 +1,8 @@
+const express = require('express');
 const ordersModel = require('../models/ordersModel');
+const router = express.Router();
 
+//em desenvolvimento
+app.get('/', request, response) => response.status(200).send('Enviado')
 
-module.exports = {
-  
-};
+module.exports = router;
