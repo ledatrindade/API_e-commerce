b8e034c878c28d1e563a50f0de145f9539d2f045
LedaTrindade <ledatrindade94@gmail.com>
2024-11-21 14:04:00 -0300

    adicionando as rotas dos produtos

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -1,5 +1,25 @@
 const productsModel = require('../models/productsModel');
 
+// Controlador para listar todos os produtos
+const getAllProducts = async (_req, res) => {
+    const products = await productsModel.getAllProducts();
+    return res.status(200).json(products);
+};
+
+// Controlador para obter um produto específico pelo ID
+const getProductById = async (req, res) => {
+    const { id } = req.params; // Pega o ID do produto da URL
+    const product = await productsModel.getProductById(id);
+
+    if (!product) {
+        return res.status(404).json({ message: 'Produto não encontrado.' });
+    }
+
+    return res.status(200).json(product);
+};
+
+// Exporte as novas funções
 module.exports = {
-  
+    getAllProducts,
+    getProductById,
 };
